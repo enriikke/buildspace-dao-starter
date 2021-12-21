4adcfde71681b9ddf8ee50ea7d8597c7f69eaff9
Enrique Gonzalez <enriikke@gmail.com>
2021-12-21 00:15:57 +0000

    revoke roles
    Signed-off-by: GitHub <noreply@github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -177,6 +177,18 @@ const App = () => {
     });
   }
 
+  if (error && error.name === "UnsupportedChainIdError") {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
